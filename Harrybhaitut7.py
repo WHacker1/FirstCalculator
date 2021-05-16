210513c7e4c2abc31bf8c5d93f695e8462206683
WHacker1 <84237695+WHacker1@users.noreply.github.com>
2021-05-16 09:24:22 +0530

    Add files via upload

---
+++ Harrybhaitut7.py
@@ -0,0 +1,9 @@
+print("Enter Your Number")
+a=input()
+print("You choosed Number =", a , "and ",a, "+ 10 is= ",int(a)+10)
+# quiz answer Making a calculator
+print("---Its a Calculator---","    To use it enter your number.   ")
+a=input()
+print("number you want to add : ")
+b=input()
+print("Your both numbers:", a,"and", b,"\nNumber after adding both the numbers you gave: ",int(a)+int(b))
